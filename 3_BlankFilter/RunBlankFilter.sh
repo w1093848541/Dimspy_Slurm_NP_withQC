ed276342d7afdfc33283ca4931a9086e63c862c8
w1093848541 <77172096+w1093848541@users.noreply.github.com>
2022-09-03 16:27:06 -0400

    Update RunBlankFilter.sh

--- 3_BlankFilter/RunBlankFilter.sh
+++ 3_BlankFilter/RunBlankFilter.sh
@@ -28,9 +28,9 @@ dimspy blank-filter \
 --input ../2_AlignSamples/alignSamples.* \
 --output blankFilter.$SLURM_JOB_ID \
 --blank-label 'blank' \
---min-fraction 1.0 \
+--min-fraction 0.85 \
 --function mean \
---min-fold-change 10.0 \
+--min-fold-change 3.0 \
 --remove-blank-samples
 
 echo "Blank Filter Complete"
